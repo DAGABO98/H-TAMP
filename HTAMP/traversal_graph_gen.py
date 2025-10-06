656e91bd92962c8ce7dcafcd6c0bc57d6250567b
DAGABO98 <dg3008@columbia.edu>
2025-10-06 12:39:25 -0700

    Add drive through extraction function

--- HTAMP/traversal_graph_gen.py
+++ HTAMP/traversal_graph_gen.py
@@ -22,14 +22,13 @@ class Corridor:
 
 @dataclass
 class DriveThrough:
-    entry_lane: Lane
-    exit_lane: Lane
     entry_start: Coordinate
     entry_end: Coordinate
     exit_start: Coordinate
     exit_end: Coordinate
     entry_corridor_id: str
     exit_corridor_id: str
+    lanes: List[Lane]
 
 @dataclass
 class Doorway:
@@ -58,11 +57,13 @@ class TraversalGraph:
 
 class TraversalGraphGenerator:
     def __init__(self, occupancy_map_path: str, config_path: str, meters_per_pixel: float = 0.036, factor: int = 1,
-                 num_lanes_per_corridor: int = 3):
+                 num_lanes_per_corridor: int = 3, num_lanes_per_drive_through: int = 1, num_lanes_per_doorway: int = 2):
         self.occupancy_map_path = occupancy_map_path
         self.config_path = config_path
         self.meters_per_cell = meters_per_pixel * factor
         self.num_lanes_per_corridor = num_lanes_per_corridor
+        self.num_lanes_per_drive_through = num_lanes_per_drive_through
+        self.num_lanes_per_doorway = num_lanes_per_doorway
         self.occupancy_map = self._load_map(occupancy_map_path)
         self.config = self._load_config(config_path)
         self.corridors = self._extract_corridors_from_config()
@@ -132,16 +133,55 @@ class TraversalGraphGenerator:
         drive_throughs = []
         if 'drive_throughs' in self.config:
             for dt in self.config['drive_throughs']:
-                if 'entry_start_point' in dt and 'entry_end_point' in dt and 'exit_start_point' in dt and 'exit_end_point' in dt:
-                    entry_start = Coordinate(x=(dt['entry_start_point'][0]*self.meters_per_cell), 
-                                             y=(dt['entry_start_point'][1]*self.meters_per_cell))
-                    entry_end = Coordinate(x=(dt['entry_end_point'][0]*self.meters_per_cell), 
-                                           y=(dt['entry_end_point'][1]*self.meters_per_cell))
-                    exit_start = Coordinate(x=(dt['exit_start_point'][0]*self.meters_per_cell), 
-                                            y=(dt['exit_start_point'][1]*self.meters_per_cell))
-                    exit_end = Coordinate(x=(dt['exit_end_point'][0]*self.meters_per_cell), 
-                                          y=(dt['exit_end_point'][1]*self.meters_per_cell))
-                    drive_throughs.append(DriveThrough(entry_start, entry_end, exit_start, exit_end))
+                dt_lanes = []
+                dt_direction = dt.get('direction', None)
+                if dt_direction is None:
+                    print("Warning: Drive-through without direction found in config.")
+                    continue
+                elif dt_direction == 'horizontal':
+                    entry_width = self.meters_per_cell * abs(dt['entry_end_point'][1] - dt['entry_start_point'][1])
+                    exit_width = self.meters_per_cell * abs(dt['exit_end_point'][1] - dt['exit_start_point'][1])
+                    entry_lane_separation = entry_width / (self.num_lanes_per_drive_through + 1)
+                    exit_lane_separation = exit_width / (self.num_lanes_per_drive_through + 1)
+                    for lane_idx in range(1, self.num_lanes_per_drive_through + 1):
+                        entry_lane_y = (self.meters_per_cell * dt['entry_start_point'][1]) + (lane_idx * entry_lane_separation)
+                        exit_lane_y = (self.meters_per_cell * dt['exit_start_point'][1]) + (lane_idx * exit_lane_separation)
+                        lane_struct = Lane(start_point=Coordinate(x=dt['entry_start_point'][0]*self.meters_per_cell, y=entry_lane_y),
+                                            end_point=Coordinate(x=dt['exit_end_point'][0]*self.meters_per_cell, y=exit_lane_y))
+                        dt_lanes.append(lane_struct)
+                elif dt_direction == 'vertical':
+                    entry_width = self.meters_per_cell * abs(dt['entry_end_point'][0] - dt['entry_start_point'][0])
+                    exit_width = self.meters_per_cell * abs(dt['exit_end_point'][0] - dt['exit_start_point'][0])
+                    entry_lane_separation = entry_width / (self.num_lanes_per_drive_through + 1)
+                    exit_lane_separation = exit_width / (self.num_lanes_per_drive_through + 1)
+                    for lane_idx in range(1, self.num_lanes_per_drive_through + 1):
+                        entry_lane_x = (self.meters_per_cell * dt['entry_start_point'][0]) + (lane_idx * entry_lane_separation)
+                        exit_lane_x = (self.meters_per_cell * dt['exit_start_point'][0]) + (lane_idx * exit_lane_separation)
+                        lane_struct = Lane(start_point=Coordinate(x=entry_lane_x, y=dt['entry_start_point'][1]*self.meters_per_cell),
+                                            end_point=Coordinate(x=exit_lane_x, y=dt['exit_end_point'][1]*self.meters_per_cell))
+                        dt_lanes.append(lane_struct)
+                else:
+                    print(f"Warning: Unknown drive-through direction '{dt_direction}' found in config.")
+                    continue
+
+                entry_start = Coordinate(x=(dt['entry_start_point'][0]*self.meters_per_cell), 
+                                         y=(dt['entry_start_point'][1]*self.meters_per_cell))
+                entry_end = Coordinate(x=(dt['entry_end_point'][0]*self.meters_per_cell), 
+                                        y=(dt['entry_end_point'][1]*self.meters_per_cell))
+                exit_start = Coordinate(x=(dt['exit_start_point'][0]*self.meters_per_cell), 
+                                        y=(dt['exit_start_point'][1]*self.meters_per_cell))
+                exit_end = Coordinate(x=(dt['exit_end_point'][0]*self.meters_per_cell), 
+                                        y=(dt['exit_end_point'][1]*self.meters_per_cell))
+                
+                current_drive_through = DriveThrough(entry_start=entry_start,
+                                                     entry_end=entry_end,
+                                                     exit_start=exit_start,
+                                                     exit_end=exit_end,
+                                                     entry_corridor_id=dt.get('entry_corridor_id', 'unknown'),
+                                                     exit_corridor_id=dt.get('exit_corridor_id', 'unknown'),
+                                                     lanes=dt_lanes)
+
+                drive_throughs.append(current_drive_through)
         return drive_throughs
 
     def _extract_doorways_from_config(self) -> List[Doorway]:
