3473984ae8d5821d5f807bb5c4398314b5a35e86
DAGABO98 <dg3008@columbia.edu>
2026-03-03 08:10:27 -0800

    Add boiler plate for base policy for adaptive rollout

--- HTAMP/assignment/policies/adaptive_rollout.py
+++ HTAMP/assignment/policies/adaptive_rollout.py
@@ -9,9 +9,10 @@ from HTAMP.planning.motion_planner import MotionPlanner
 from HTAMP.planning.planning_dataclasses import NodeReservationTable, RequestsLists, TaskRequest, TimeReservation
 from HTAMP.planning.state import PlanningState
 
-class AdaptiveRollout:
+class SequentialGreedyBasePolicy:
     def __init__(self, allow_deallocation: bool = True):
-        self.requests_queue = TaskQueue()
+        self.real_requests_queue = TaskQueue()
+        self.predicted_requests_queue = TaskQueue()
         self.dummy_delivery_robot_profile = RobotProfile(radius=0.10, speed=0.20, robot_id=-1, robot_type="delivery")
         self.assigned_requests:  dict[int, list[str]]  = {}
         self.node_reservation_table = NodeReservationTable(reservations={},
@@ -653,3 +654,11 @@ class AdaptiveRollout:
                                             motion_planner=motion_planner,
                                             traversal_graph_generator=traversal_graph_generator,
                                             debug=debug)
+
+class AdaptiveRollout:
+    def __init__(self, allow_deallocation: bool = True):
+        self.requests_queue = TaskQueue()
+        self.dummy_delivery_robot_profile = RobotProfile(radius=0.10, speed=0.20, robot_id=-1, robot_type="delivery")
+        self.assigned_requests:  dict[int, list[str]]  = {}
+        self.node_reservation_table = NodeReservationTable(reservations={},
+                                                          robot_node_dict={})
