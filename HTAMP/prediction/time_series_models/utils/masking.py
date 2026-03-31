2d02d0a84678585c9c5908274e0a26427c91982d
DAGABO98 <dg3008@columbia.edu>
2026-03-31 10:09:14 -0700

    Add typing to prediction utils

--- HTAMP/prediction/time_series_models/utils/masking.py
+++ HTAMP/prediction/time_series_models/utils/masking.py
@@ -1,25 +1,41 @@
 import torch
+from torch import Tensor
 
-class TriangularCausalMask():
-    def __init__(self, B, L, device="cpu"):
-        mask_shape = [B, 1, L, L]
+
+class TriangularCausalMask:
+    def __init__(self, B: int, L: int, device: str = "cpu") -> None:
+        mask_shape = (B, 1, L, L)
         with torch.no_grad():
-            self._mask = torch.triu(torch.ones(mask_shape, dtype=torch.bool), diagonal=1).to(device)
+            self._mask: Tensor = torch.triu(
+                torch.ones(mask_shape, dtype=torch.bool),
+                diagonal=1,
+            ).to(device)
 
     @property
-    def mask(self):
+    def mask(self) -> Tensor:
         return self._mask
 
 
-class ProbMask():
-    def __init__(self, B, H, L, index, scores, device="cpu"):
+class ProbMask:
+    def __init__(
+        self,
+        B: int,
+        H: int,
+        L: int,
+        index: Tensor,
+        scores: Tensor,
+        device: str = "cpu",
+    ) -> None:
         _mask = torch.ones(L, scores.shape[-1], dtype=torch.bool).to(device).triu(1)
         _mask_ex = _mask[None, None, :].expand(B, H, L, scores.shape[-1])
-        indicator = _mask_ex[torch.arange(B)[:, None, None],
-                    torch.arange(H)[None, :, None],
-                    index, :].to(device)
-        self._mask = indicator.view(scores.shape).to(device)
+        indicator = _mask_ex[
+            torch.arange(B)[:, None, None],
+            torch.arange(H)[None, :, None],
+            index,
+            :,
+        ].to(device)
+        self._mask: Tensor = indicator.view(scores.shape).to(device)
 
     @property
-    def mask(self):
+    def mask(self) -> Tensor:
         return self._mask
--- HTAMP/prediction/time_series_models/utils/metrics.py
+++ HTAMP/prediction/time_series_models/utils/metrics.py
@@ -1,26 +1,33 @@
-import numpy as np
+from typing import Tuple
 
-def MAE(pred, true):
+import numpy as np
+import numpy.typing as npt
+
+
+NDArrayFloat = npt.NDArray[np.floating]
+
+
+def MAE(pred: NDArrayFloat, true: NDArrayFloat) -> np.floating:
     return np.mean(np.abs(true - pred))
 
 
-def MSE(pred, true):
+def MSE(pred: NDArrayFloat, true: NDArrayFloat) -> np.floating:
     return np.mean((true - pred) ** 2)
 
 
-def RMSE(pred, true):
+def RMSE(pred: NDArrayFloat, true: NDArrayFloat) -> np.floating:
     return np.sqrt(MSE(pred, true))
 
 
-def SMAPE(pred, true):
-    return np.mean(np.abs((true - pred)) / ((np.abs(true) + np.abs(pred))/2))
+def SMAPE(pred: NDArrayFloat, true: NDArrayFloat) -> np.floating:
+    return np.mean(np.abs(true - pred) / ((np.abs(true) + np.abs(pred)) / 2))
 
 
-def MSPE(pred, true):
+def MSPE(pred: NDArrayFloat, true: NDArrayFloat) -> np.floating:
     return np.mean(np.square((true - pred) / true))
 
 
-def metric(pred, true):
+def metric(pred: NDArrayFloat, true: NDArrayFloat) -> Tuple[np.floating, np.floating, np.floating]:
     mae = MAE(pred, true)
     mse = MSE(pred, true)
     rmse = RMSE(pred, true)
