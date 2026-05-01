Fix issue with mixed precision for base layer

HTAMP/prediction/point_process_models/easyTPP/torch_baselayer.py
@@ -9,7 +9,8 @@ def attention(query, key, value, mask=None, dropout=None):
     scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
     if mask is not None:
         # small change here -- we use "1" for masked element
-        scores = scores.masked_fill(mask > 0, -1e9)
+        mask_fill_value = torch.finfo(scores.dtype).min
+        scores = scores.masked_fill(mask > 0, mask_fill_value)
     p_attn = torch.softmax(scores, dim=-1)
     if dropout is not None:
         p_attn = dropout(p_attn)